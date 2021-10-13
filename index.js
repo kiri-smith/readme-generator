072691d0d57652284ec5fd9a66b09def876e4f9e
Kiri <kirilynne@gmail.com>
2021-10-13 04:42:58 -0400

    add notes

---
+++ index.js
@@ -0,0 +1,44 @@
+//There are sections in the README entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+
+//WHEN I enter the title of my project then this is displayed as the title of the README
+
+//WHEN I enter a description then this information is added to the section of the README entitled Description
+
+//WHEN I enter installation instructions then this information is added to the section of the README entitled Installation
+
+//WHEN I enter usage information then this information is added to the section of the README entitled Usage
+
+//WHEN I enter contribution guidelines then this information is added to the section of the README entitled Contributing
+
+//WHEN I enter test instructions then this information is added to the section of the README entitled Tests
+
+//WHEN I choose a license for my application from a list of options THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+
+//WHEN I enter my GitHub username THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+
+//WHEN I enter my email address THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+
+//WHEN I click on the links in the Table of Contents THEN I am taken to the corresponding section of the README
+
+//title
+//description
+//installation instructions
+//usage information
+//contribution guidelines
+//test instructions
+//github username
+//email address
+//choose license from a list
+
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: '',
+            name: '',
+        }
+    ])
+
+
