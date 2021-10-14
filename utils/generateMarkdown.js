aedb3a23b02d376789df401dba2cdcc071487194
Kiri <kirilynne@gmail.com>
2021-10-14 07:04:25 -0400

    update license functions to split in 3

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,49 +1,74 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 
-var mitLicense = [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+var mitLicense = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
 
-var gnuLicense = [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+var gnuLicense = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
 
-var eclipseLicense = [![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)
+var eclipseLicense = '[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)'
 
 
 
-    function renderLicenseBadge(license) {
+function renderLicenseBadge(license) {
 
-        if (questions.choices[0]) {
+    if (license === 'MIT License') {
 
-            return mitLicense;
+        return mitLicense;
 
-        } else if (questions.choices[1]) {
+    } else if (license === 'GNU General Public License') {
 
-            return gnuLicense;
+        return gnuLicense;
 
-        } else if (questions.choices[2]) {
+    } else if (license === 'Eclipse Public License') {
 
-            return eclipseLicense;
+        return eclipseLicense;
 
-        } else {
-            return "";
-        }
+    } else {
+        return "";
     }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
 
+    if (license === 'MIT License') {
+
+        return 'https://opensource.org/licenses/MIT';
+
+    } else if (license === 'GNU General Public License') {
+
+        return 'https://www.gnu.org/licenses/gpl-3.0';
+
+    } else if (license === 'Eclipse Public License') {
+
+        return 'https://opensource.org/licenses/EPL-1.0';
+
+    } else {
+        return "";
     }
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+function renderLicenseSection(license) {
+
+    if (license === 'none') {
+        return "";
+    } else {
+        return `## License
+        ${data.license}
+        `;
+    }
+}
 
 
 
 // TODO: Create a function to generate markdown for README
 // text literal using backticks ... it allows us to combine html or text with js or jquery 
 function generateMarkdown(data) {
-        return `
+    return `
 
 
 # ${data.title}
@@ -91,6 +116,6 @@ You may also follow me on Github, to view other repositories:
 ${data.username}
 
 `;
-    }
+}
 
 module.exports = generateMarkdown;
